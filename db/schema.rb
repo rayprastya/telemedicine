aa18751aa26803dc166cd8bd5b4e03d18c379164
rezky126 <rezky127@gmail.com>
2022-07-03 12:00:55 +0700

    Add diagnosis, result page

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_170807) do
+ActiveRecord::Schema.define(version: 2022_06_30_140604) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.string "description"
-    t.string "author"
+    t.text "description"
+    t.text "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "diagnoses", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "medicines", force: :cascade do |t|
     t.string "name"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
