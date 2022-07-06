86716cb5d46349c209b8b3448f28ce2a62a273b6
rayprastya <54200114+rayprastya@users.noreply.github.com>
2022-07-06 00:31:53 +0000

    refactor : user scaffold with account reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ApplicationRecord
-  # adds virtual attributes for authentication
-  has_secure_password
-  # validates email
-  validates :username, presence: true, uniqueness: true
-  # validates :username, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid username' }
+  belongs_to :account
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_03_141557) do
+ActiveRecord::Schema.define(version: 2022_07_06_002158) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "username"
@@ -70,18 +70,6 @@ ActiveRecord::Schema.define(version: 2022_07_03_141557) do
   end
 
   create_table "telemedicines", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "nama"
-    t.string "email"
-    t.string "password_digest", null: false
-    t.integer "nomor_hp"
-    t.string "jenis_kelamin"
-    t.string "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
