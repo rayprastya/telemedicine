545938e1f09c7f915de908bb5969aa67715419df
rayprastya <54200114+rayprastya@users.noreply.github.com>
2022-07-06 00:01:34 +0000

    refactor : changing users table

--- db/migrate/20220615032628_create_users.rb
+++ db/migrate/20220615032628_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.references :accounts, null: false, foreign_key: true
       t.string :username, null: false
       t.string :nama, null: true
       t.string :email, null: true
